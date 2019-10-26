4c52caededad1964d66aa51d8af6e3d41163a732
varlackc <varlackc@mail.gvsu.edu>
2019-10-26 16:54:38 -0500

    update the dealer class
    Update the dealer class

--- Resources/Dealer.cs
+++ Resources/Dealer.cs
@@ -6,5 +6,31 @@ namespace BlackJackGame.Resources
 {
     class Dealer
     {
+        public List<Card> hand = new List<Card>();
+
+        public Dealer()
+        {
+            return;
+        }
+
+        public void AddCardToHand(Card card)
+        {
+            hand.Add(card);
+        }
+
+        public void ShowHand()
+        {
+
+            Console.WriteLine("Show The Player Cards At Hand: ");
+            for (int i = 0; i < (hand.Count); i++)
+            {
+                Console.WriteLine(hand[i]);//show element in the hand
+            }
+        }
+
+        public void DiscardHand()
+        {
+            hand.Clear();
+        }
     }
 }
