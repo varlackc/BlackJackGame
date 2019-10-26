2c0ffbe266e337070db6eaf47be9149869565450
varlackc <varlackc@mail.gvsu.edu>
2019-10-26 13:52:35 -0500

    Improve the shoe class
    Improve the shoe class

--- Program.cs
+++ Program.cs
@@ -8,47 +8,11 @@ namespace BlackJackGame
         // execute app
         static void Main(string[] args)
         {
+            //Call the shoe of cards
+            Shoe shoeOfCards = new Shoe();
 
-            Card[] hand = new Card[5]; // declare a card  to represent the hand 
-
-            var myDeckOfCards = new Resources.DeckOfCards();
-            myDeckOfCards.Shuffle(); // place Cards in random order
-
-            // Explain the program to the user
+            //Explain the program to the user
             ExplainToUser();
-
-            // display all 52 Cards in the order which they are dealt
-            for (int i = 0; i < 52; ++i)
-            {
-                var tempCarValue = myDeckOfCards.DealCard(); // create a temporary card value that holds the dealed card
-                int handPosition = i % hand.Length; //get the position the card in the hand
-                hand[handPosition] = tempCarValue; // set the card in the hand
-                Console.Write($"{ tempCarValue,-19}");//display element 
-                                                      // determines the size of the hand
-                if ((i + 1) % 5 == 0)
-                {
-                    //Run the Hand operations in this code section
-                    Console.WriteLine("\n");
-
-                    //var handResult = DeckOfCards.ContainsPair(hand);//Check if the hand contains pairs 
-                    //Console.WriteLine("Contains a Pair: " + handResult);
-                    //handResult = DeckOfCards.ContainsTwoPair(hand);//Check if the hand contains pairs 
-                    //Console.WriteLine("Contains two Pairs: " + handResult);
-                    //handResult = DeckOfCards.ContainsThreeOfKind(hand);
-                    //Console.WriteLine("Contains three Cards Of Same Type: " + handResult);
-                    //handResult = DeckOfCards.ContainsFourOfKind(hand);
-                    //Console.WriteLine("Contains Four Cards Of Same Type: " + handResult);
-                    //handResult = DeckOfCards.ContainsFlush(hand);
-                    //Console.WriteLine("Contains A Flush: " + handResult);
-                    //handResult = DeckOfCards.ContainsStraight(hand);
-                    //Console.WriteLine("Contains A Straight: " + handResult);
-                    //handResult = DeckOfCards.ContainsFullHouse(hand);
-                    //Console.WriteLine("Contains Contains Full House: " + handResult);
-                    //Console.WriteLine("\n\n");
-                }
-            }
-
-            Console.Read(); //wait until the user closes the program
 
 
         }
--- Resources/Shoe.cs
+++ Resources/Shoe.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace BlackJackGame.Resources
 {
@@ -9,37 +10,33 @@ namespace BlackJackGame.Resources
         private static int  deckSize = 52;
         private static int numberOfDecks = 6;
         private static int  shoeSize = deckSize * numberOfDecks;
-        Card[] shoeOfCards = new Card[shoeSize];
+        private List<Card> shoeOfCards = new List<Card>(); //Create an empty list of cards to produce the shoe
 
         public Shoe() {
+            //Declare the card information
+            string[] faces = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+            string[] suits = { "Hears", "Spades", "Diamonds", "Clubs" };
 
-            // create the shoe
-
-
-            //create deck
-            var myDeckOfCards = new DeckOfCards();
-
-            //update the shoe of cards
-            for (int i = 0; i < shoeSize; ++i)
-            {
-                    Card tempCardValue = myDeckOfCards.DealCard();
-                for (int j = 0; j < deckSize; ++j)
-                {
-                    shoeOfCards[i] = tempCardValue;
-
-                }
-            }
-
-            for (int i = 0; i < shoeSize; ++i)
-            {
-
-            }
-
-            //populate the shoe
             
 
+            //populate the shoe
+            for (int i = 0; i < shoeSize; i++) {
+                Card currentCard = new Card(faces[i%13], suits[i%4]);//create new card
+                shoeOfCards.Add(currentCard);//add the current card to the shoe
+            }
+
+            /*
+            // show the elements in the shoe
+            for (int i = 0; i < shoeSize; i++) {
+                Console.WriteLine(shoeOfCards.ElementAt(i));
+            }
+            */
+
+            //Add a section to create the shoe, it should be a list of cards
+
+            //
 
         }
-         
+
     }
 }
